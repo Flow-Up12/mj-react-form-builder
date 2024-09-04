0c66a2b666a1440e473f145f17bd7f97794e9a3b
Flow-Up12 <marcosje2005@icloud.com>
2024-09-03 21:06:18 -0500

    huge styling improvements to the ContactArrayInput changed location of the add/delete button and added a grid for styling

--- src/components/ContactArrayInput.tsx
+++ src/components/ContactArrayInput.tsx
@@ -1,7 +1,9 @@
-import { useFieldArray, useFormContext } from "react-hook-form";
 import { useState } from "react";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import DeleteIcon from '@mui/icons-material/Delete';
 import { TextInput } from "./TextInput";
-import {MaskedPhoneInput} from "./MaskedPhoneInput";
+import { MaskedPhoneInput } from "./MaskedPhoneInput";
+
 
 interface ContactArrayProps {
   source: string;
@@ -29,10 +31,7 @@ export const ContactArrayInput = ({
     }));
 
     if (isChecked) {
-      setValue(
-        `${path}.email`,
-        `anonymous+${firstName}${lastName}@orwa.org`
-      );
+      setValue(`${path}.email`, `anonymous+${firstName}${lastName}@orwa.org`);
     } else {
       setValue(`${path}.email`, "");
     }
@@ -46,75 +45,88 @@ export const ContactArrayInput = ({
 
     return (
       <div>
-        <p className="text-left text-lg font-semibold">{label}</p>
-        <hr className="my-4 text-black bg-black" />
-        {fields.map((field, index) => (
-          <div key={field.id} className="border p-4 mb-4 rounded-md">
-            <TextInput
-              source={`${source}[${index}].first`}
-              label="First source"
-              required
-            />
-            <TextInput
-              source={`${source}[${index}].last`}
-              label="Last source"
-              required
-            />
-            <div className="flex just">
-              <input
-                type="checkbox"
-                id={`anonymous-email-${index}`}
-                onChange={(e) => handleAnonymousEmail(`${source}[${index}]`, e.target.checked)}
-              />
-              <label htmlFor={`anonymous-email-${index}`} className="ml-2">
-                Don't have an email
-              </label>
-            </div>
-            {!anonymousEmails[`${source}[${index}]`] && (
-              <TextInput
-                source={`${source}[${index}].email`}
-                label="Email"
-                type="email"
-                required
-              />
-            )}
-            <MaskedPhoneInput source={`${source}[${index}].phone`} />
-            <button
-              type="button"
-              onClick={() => remove(index)}
-              className="mt-2 bg-red-500 text-white p-2 rounded-md"
+        <div className="flex justify-between items-center mb-4">
+          <p className="text-left text-lg font-semibold">{label}</p>
+          <button
+            type="button"
+            onClick={() => append({ first: "", last: "", email: "", phone: "" })}
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition"
+          >
+            Add Contact
+          </button>
+        </div>
+        <hr className="my-4" />
+        <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+          {fields.map((field, index) => (
+            <div
+              key={field.id}
+              className="relative border border-gray-300 shadow-sm p-6 rounded-lg bg-white"
             >
-              Remove Contact
-            </button>
-          </div>
-        ))}
-        <button
-          type="button"
-          onClick={() => append({ first: "", last: "", email: "", phone: "" })}
-          className="bg-blue-500 text-white p-2 rounded-md"
-        >
-          Add Contact
-        </button>
-        <p className="text-sm text-gray-500 text-left">{helperText}</p>
+              <button
+                type="button"
+                onClick={() => remove(index)}
+                className="absolute top-2 right-2 bg-red-500 text-white pb-1 px-3 rounded-md hover:bg-red-600 transition align-middle"
+              >
+                <DeleteIcon fontSize='small'/>
+              </button>
+              <div className="grid grid-cols-1 gap-4">
+                <TextInput
+                  source={`${source}[${index}].first`}
+                  label="First Name"
+                  required
+                />
+                <TextInput
+                  source={`${source}[${index}].last`}
+                  label="Last Name"
+                  required
+                />
+              </div>
+              <div className="flex items-center mt-4">
+                <input
+                  type="checkbox"
+                  id={`anonymous-email-${index}`}
+                  onChange={(e) => handleAnonymousEmail(`${source}[${index}]`, e.target.checked)}
+                  className="mr-2"
+                />
+                <label htmlFor={`anonymous-email-${index}`} className="text-sm text-gray-600">
+                  Don&apos;t have an email
+                </label>
+              </div>
+              {!anonymousEmails[`${source}[${index}]`] && (
+                <TextInput
+                  source={`${source}[${index}].email`}
+                  label="Email"
+                  type="email"
+                  required
+                />
+              )}
+              <MaskedPhoneInput
+                source={`${source}[${index}].phone`}
+              />
+            </div>
+          ))}
+        </div>
+        {helperText && (
+          <p className="text-sm text-gray-500 text-left mt-4">{helperText}</p>
+        )}
       </div>
     );
   } else {
     return (
-      <div className="p-4 mb-4 rounded-md">
+      <div className="p-6 border border-gray-300 shadow-sm rounded-lg bg-white">
         <p className="text-left text-lg font-semibold">{label}</p>
         <hr className="my-2" />
-        <TextInput source={`${source}.first`} label="First source" required />
-        <TextInput source={`${source}.last`} label="Last source" required />
-        <div className="flex items-center">
+        <TextInput source={`${source}.first`} label="First Name" required />
+        <TextInput source={`${source}.last`} label="Last Name" required />
+        <div className="flex items-center mt-4">
           <input
             type="checkbox"
             id="anonymous-email"
-            onChange={(e) =>
-              handleAnonymousEmail(source, e.target.checked)
-            }
+            onChange={(e) => handleAnonymousEmail(source, e.target.checked)}
+            className="mr-2"
           />
-          <label htmlFor="anonymous-email" className="ml-2">
-            Don't have an email
+          <label htmlFor="anonymous-email" className="text-sm text-gray-600">
+            Don&apos;t have an email
           </label>
         </div>
         {!anonymousEmails[source] && (
@@ -125,9 +137,8 @@ export const ContactArrayInput = ({
             required
           />
         )}
-        <MaskedPhoneInput source={`${source}.phone`} />
-        <TextInput source={`${source}.title`} label="Title" required />
-        <p className="text-sm text-gray-500 text-left">{helperText}</p>
+        <MaskedPhoneInput source={`${source}.phone`}/>
+        <p className="text-sm text-gray-500 text-left mt-2">{helperText}</p>
       </div>
     );
   }
