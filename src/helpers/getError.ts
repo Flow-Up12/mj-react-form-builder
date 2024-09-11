772baa45ad388fd8d2286a2ed3eb7b3e9c825dff
Flow-Up12 <marcosje2005@icloud.com>
2024-09-11 11:40:22 -0500

    updated getErrors function to handle arrays instead of only objects

--- src/helpers/getError.ts
+++ src/helpers/getError.ts
@@ -1,14 +1,19 @@
 import { useFormContext } from "react-hook-form";
 
-export const getError = (name: string,) => {
+export const getError = (name: string) => {
+  const {
+    formState: { errors },
+  } = useFormContext();
 
-    const { formState: { errors } } = useFormContext();
-
-  const errorPath = name.split(".").reduce((acc, key) => {
-    return acc?.[key] || acc?.[parseInt(key)] || {};
-  }, errors as any);
+  const errorPath = name.split('.').reduce((acc, key) => {
+    // Handle case where key might represent an array index
+    if (acc && typeof acc === 'object' && key in acc) {
+      return acc[key];
+    } else if (acc && Array.isArray(acc)) {
+      return acc[parseInt(key, 10)];
+    }
+    return undefined;
+  }, errors);
 
   return errorPath?.message;
 };
-
-// gets errors for arrays and regular inputs
